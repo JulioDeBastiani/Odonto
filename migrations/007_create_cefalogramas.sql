54d418ce1b2e859099828e7346e4332c1bf91961
Julio De Bastiani <julioc.debastiani@gmail.com>
2019-07-10 20:48:07 -0300

    detalhe no banco

--- migrations/007_create_cefalogramas.sql
+++ migrations/007_create_cefalogramas.sql
@@ -6,21 +6,7 @@ CREATE TABLE Cefalogramas (
     MotivoExame VARCHAR(255),
     ParametrizacaoId INT NOT NULL,
     Modelo INT NOT NULL, -- 1..4
-
-    TipoPanoramica VARCHAR (30) NOT NULL, -- 'Normal', 'Infantil', 'Implantes', 'Telerradiografia Lateral', 'Telerradiografia Frontal', 'Radiografia Carpal', 'Radiografia Atm Planigrafia', 'Radiografia Seios Maxilares'
-    Especializacao VARCHAR (15), -- 'Topo', 'Oclusao', 'Idade Ossea'
-    Regiao VARCHAR(50),
-    Modelo INT, -- 1..9
     PRIMARY KEY (Id),
-
-
-
-        public ModeloCefalograma Modelo { get; set; }
-        public int Medida { get; set; }
-
-
-
-
     INDEX pac_cef_ind (PacienteId),
     FOREIGN KEY (PacienteId)
         REFERENCES Pacientes (Id)
